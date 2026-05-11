f68560c321474db5886ea5e2b862d24987cb47e9
Moshe Immerman <moshe@flanksource.com>
2026-05-11 17:10:45 +0200

    build(makefile): add git commit and build date to binary via ldflags
    Add GIT_COMMIT and BUILD_DATE variables to Makefile that are injected into the binary at build time using ldflags. This allows the binary to report its commit hash and build timestamp.
    
    Also reorganize imports in example webapp to consolidate EntityExplorerApp import and remove unused path aliases from tsconfig and vite config.

--- examples/enitity/webapp/src/App.tsx
+++ examples/enitity/webapp/src/App.tsx
@@ -1,5 +1,4 @@
 import { Link, Routes, Route, useLocation } from "react-router-dom";
-import { EntityExplorerApp } from "@flanksource/clicky-ui/api-explorer";
 import {
   DensitySwitcher,
   EntityExplorerApp,
